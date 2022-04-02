faa23112a46b769070cad483c22f4fc5debe7d6d
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-02 19:44:07 -0400

    Updating CI and CD pipelines for sonar scanning.

---
+++ jest.release.config.ts
@@ -0,0 +1,17 @@
+import { Config } from '@jest/types'
+
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    passWithNoTests: false,
+    collectCoverage: true,
+    coverageDirectory: 'test_results/coverage',    
+    testRegex: '(?<!i)\.(spec|test)\.[jt]s$'
+  }
+};
