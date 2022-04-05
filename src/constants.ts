26c0cf5b167a5a92c6a98279a91895d906bbf71f
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-04 21:19:58 -0400

    Updating platform to ensure it loads correctly.

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+/**
+ * The name of the platform (which must match the config.schema.json pluginAlias field).
+ */
+ export const PLATFORM_NAME = "Homebridge Automower Platform";
+
+ /**
+  * The name of the plugin (which must match the package.json package name).
+  */
+ export const PLUGIN_NAME = "homebridge-automower-platform";
+ 
+ /**
+  * The base url for the authentication api.
+  */
+ export const AUTHENTICATION_API_BASE_URL = "https://api.authentication.husqvarnagroup.dev/v1";
+ 
+ /**
+  * The base url for the Automower Connect api.
+  */
+ export const AUTOMOWER_CONNECT_API_BASE_URL = "https://api.amc.husqvarna.dev/v1";
