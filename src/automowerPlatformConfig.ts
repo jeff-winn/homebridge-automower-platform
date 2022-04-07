82b067fdb309188a2004112de38952408bc22799
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-07 08:40:19 -0400

    Cleanup.

--- src/automowerPlatformConfig.ts
+++ src/automowerPlatformConfig.ts
@@ -1,9 +1,9 @@
-import { PlatformConfig } from "homebridge";
+import { PlatformConfig } from 'homebridge';
 
 /** 
  * Describes the platform configuration settings.
  */
- export interface AutomowerPlatformConfig extends PlatformConfig {    
+export interface AutomowerPlatformConfig extends PlatformConfig {    
     username: string;
     password: string;
     appKey: string;    
