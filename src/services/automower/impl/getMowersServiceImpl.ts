26c93c9a305854ca7483c4efb01e00a46dbb1996
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-08 21:14:50 -0400

    Cleanup.

--- src/services/automower/impl/getMowersServiceImpl.ts
+++ src/services/automower/impl/getMowersServiceImpl.ts
@@ -1,6 +1,6 @@
-import { OAuthTokenManager } from "../../../authentication/oauthTokenManager";
-import { AutomowerClient, Mower } from "../../../clients/automowerClient";
-import { GetMowersService } from "../getMowersService";
+import { OAuthTokenManager } from '../../../authentication/oauthTokenManager';
+import { AutomowerClient, Mower } from '../../../clients/automowerClient';
+import { GetMowersService } from '../getMowersService';
 
 export class GetMowersServiceImpl implements GetMowersService {
     constructor(private tokenManager: OAuthTokenManager, private client: AutomowerClient) { }
