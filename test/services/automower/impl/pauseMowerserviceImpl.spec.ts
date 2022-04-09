86f88993a34d30dceefb8ff277a45ea50cab566f
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-08 22:06:13 -0400

    Cleanup.

--- test/services/automower/impl/pauseMowerserviceImpl.spec.ts
+++ test/services/automower/impl/pauseMowerserviceImpl.spec.ts
@@ -1,10 +1,10 @@
-import { OAuthTokenManager } from "../../../../src/authentication/oauthTokenManager"
-import { AutomowerClient } from '../../../../src/clients/automowerClient'
-import { PauseMowerServiceImpl } from '../../../../src/services/automower/impl/pauseMowerServiceImpl'
-import { Mock, It, Times } from 'moq.ts'
-import { OAuthToken } from "../../../../src/clients/authenticationClient";
+import { OAuthTokenManager } from '../../../../src/authentication/oauthTokenManager';
+import { AutomowerClient } from '../../../../src/clients/automowerClient';
+import { PauseMowerServiceImpl } from '../../../../src/services/automower/impl/pauseMowerServiceImpl';
+import { Mock, It, Times } from 'moq.ts';
+import { OAuthToken } from '../../../../src/clients/authenticationClient';
 
-describe("pause mower service", () => {
+describe('pause mower service', () => {
     let tokenManager: Mock<OAuthTokenManager>;
     let client: Mock<AutomowerClient>;
 
@@ -17,21 +17,21 @@ describe("pause mower service", () => {
         target = new PauseMowerServiceImpl(tokenManager.object(), client.object());
     });
 
-    it("should pause the mower", async () => {
-        const id = "12345";
+    it('should pause the mower', async () => {
+        const id = '12345';
         const token: OAuthToken = {
-            access_token: "12345",
+            access_token: '12345',
             expires_in: 1000,
-            provider: "husqvarna",
-            refresh_token: "abcd12345",
-            scope: "abcd",
-            token_type: "Bearer",
-            user_id: "me"
+            provider: 'husqvarna',
+            refresh_token: 'abcd12345',
+            scope: 'abcd',
+            token_type: 'Bearer',
+            user_id: 'me'
         };
 
         tokenManager.setup(x => x.getCurrentToken()).returns(Promise.resolve(token));
         client.setup(x => x.doAction(id, {
-            type: "Pause"
+            type: 'Pause'
         }, token)).returns(Promise.resolve(undefined));
 
         await target.pauseMowerById(id);
