98431221abbef29fdac38ddcced240a8b1caf033
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-09 14:27:40 -0400

    Adding DI container support.

--- src/automowerPlatform.ts
+++ src/automowerPlatform.ts
@@ -1,65 +1,62 @@
-import * as homebridge from 'homebridge';
-import { PlatformAccessory } from 'homebridge';
+import { PlatformAccessory, API, DynamicPlatformPlugin, PlatformConfig, APIEvent, Logging } from 'homebridge';
 import { AutomowerPlatformConfig } from './automowerPlatformConfig';
-import { PLUGIN_NAME, PLATFORM_NAME } from './constants';
+import { AutomowerPlatformContainer } from './automowerPlatformContainer';
+import { GetMowersService } from './services/automower/getMowersService';
+import { GetMowersServiceImpl } from './services/automower/impl/getMowersServiceImpl';
 
-let Accessory: typeof PlatformAccessory;
+export class AutomowerPlatform implements DynamicPlatformPlugin {
+    private readonly accessories: PlatformAccessory[] = [];
 
-export class AutomowerPlatform implements homebridge.DynamicPlatformPlugin {
-    private readonly hap: homebridge.HAP;
-    private readonly accessories: homebridge.PlatformAccessory[] = [];
     private readonly config: AutomowerPlatformConfig;
+    private readonly container: AutomowerPlatformContainer;        
 
-    constructor(private log: homebridge.Logging, config: homebridge.PlatformConfig, private api: homebridge.API) {
-        this.hap = api.hap;
+    constructor(private log: Logging, config: PlatformConfig, private api: API) {
         this.config = config as AutomowerPlatformConfig;
+        this.container = new AutomowerPlatformContainer(this.config, this.log);
 
-        api.on(homebridge.APIEvent.DID_FINISH_LAUNCHING, async () => {
-            log.info('Example platform \'didFinishLaunching\'');
+        api.on(APIEvent.DID_FINISH_LAUNCHING, () => {            
+            this.onFinishedLaunching();
         });
+
+        api.on(APIEvent.SHUTDOWN, () => {
+            this.onShutdown();
+        });
+    }
+
+    private async onFinishedLaunching(): Promise<void> {
+        this.container.registerEverything();        
+
+        await this.discoverMowers();
+
+        this.log.debug('onFinishLaunching');
+    }
+    
+    private async discoverMowers(): Promise<void> {
+        this.log.info('Discovering available mowers...');
+
+        const mowerService = this.getMowerService();
+        const mowers = await mowerService.getMowers();
+        
+        mowers?.forEach(mower => {
+            this.log.info(`Found mower: ${mower.attributes.system.name}`);
+        });
+
+        this.log.info('Completed mower discovery.');
+    }
+
+    protected getMowerService(): GetMowersService {
+        return this.container.resolve(GetMowersServiceImpl);
+    }
+
+    private onShutdown(): void {
+        this.log.debug('onShutdown');
     }
 
     /*
      * This function is invoked when homebridge restores cached accessories from disk at startup.
      * It should be used to setup event handlers for characteristics and update respective values.
      */
-    configureAccessory(accessory: homebridge.PlatformAccessory): void {
-        this.log('Configuring accessory %s', accessory.displayName);
-
-        accessory.on(homebridge.PlatformAccessoryEvent.IDENTIFY, () => {
-            this.log('%s identified!', accessory.displayName);
-        });
-
-        accessory.getService(this.hap.Service.Lightbulb)!.getCharacteristic(this.hap.Characteristic.On)
-            .on(homebridge.CharacteristicEventTypes.SET, 
-                (value: homebridge.CharacteristicValue, callback: homebridge.CharacteristicSetCallback) => {
-                    this.log.info('%s Light was set to: ' + value);
-                    callback();
-                });
-
-        this.accessories.push(accessory);
-    }
-
-    // --------------------------- CUSTOM METHODS ---------------------------
-
-    addAccessory(name: string) {
-        this.log.info('Adding new accessory with name %s', name);
-
-        // uuid must be generated from a unique but not changing data source, name should not be used in the most cases.
-        const uuid = this.hap.uuid.generate(name);
-        const accessory = new Accessory(name, uuid);
-
-        accessory.addService(this.hap.Service.Lightbulb, 'Test Light');
-
-        this.configureAccessory(accessory); // abusing the configureAccessory here
-
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-    }
-
-    removeAccessories() {
-        this.log.info('Removing all accessories');
-
-        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, this.accessories);
-        this.accessories.splice(0, this.accessories.length); // clear out the array
-    }
+    configureAccessory(accessory: PlatformAccessory): void {
+        this.log.info(`Configuring accessory ${accessory.displayName}`);
+    }    
 }
---
+++ src/automowerPlatformContainer.ts
@@ -0,0 +1,61 @@
+import { Logging } from 'homebridge';
+import { container, DependencyContainer, InjectionToken } from 'tsyringe';
+import { OAuthTokenManagerImpl } from './authentication/impl/oauthTokenManagerImpl';
+import { AutomowerPlatformConfig } from './automowerPlatformConfig';
+import { AuthenticationClientImpl } from './clients/impl/authenticationClientImpl';
+import { AutomowerClientImpl } from './clients/impl/automowerClientImpl';
+import { GetMowersServiceImpl } from './services/automower/impl/getMowersServiceImpl';
+import { PauseMowerServiceImpl } from './services/automower/impl/pauseMowerServiceImpl';
+import * as constants from './constants';
+
+export class AutomowerPlatformContainer {
+    private readonly container: DependencyContainer;
+
+    constructor(private config: AutomowerPlatformConfig, private log: Logging) { 
+        this.container = container;
+    }
+
+    registerEverything(): void {
+        this.log.debug('Registering classes to the DI container...');
+
+        this.container.register(AutomowerClientImpl, {
+            useValue: new AutomowerClientImpl(
+                this.config.appKey,
+                constants.AUTOMOWER_CONNECT_API_BASE_URL
+            )
+        });
+    
+        this.container.register(AuthenticationClientImpl, {
+            useValue: new AuthenticationClientImpl(
+                this.config.appKey,
+                constants.AUTHENTICATION_API_BASE_URL
+            )
+        });
+    
+        this.container.register(OAuthTokenManagerImpl, {
+            useFactory: (context) => new OAuthTokenManagerImpl(
+                context.resolve(AuthenticationClientImpl),
+                this.config)
+        });
+
+        this.container.register(GetMowersServiceImpl, {
+            useFactory: (context) => new GetMowersServiceImpl(
+                context.resolve(OAuthTokenManagerImpl),
+                context.resolve(AutomowerClientImpl)
+            )
+        });
+
+        this.container.register(PauseMowerServiceImpl, {
+            useFactory: (context) => new PauseMowerServiceImpl(
+                context.resolve(OAuthTokenManagerImpl),
+                context.resolve(AutomowerClientImpl)
+            )
+        });
+
+        this.log.debug('Completed DI container registrations.');
+    }
+
+    resolve<T>(token: InjectionToken<T>): T {
+        return this.container.resolve(token);
+    }
+}
