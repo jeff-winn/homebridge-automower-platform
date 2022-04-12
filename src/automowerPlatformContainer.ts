2c112f5a9479e8b16d796c754097a003f8b59d1d
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-04-11 22:15:41 -0400

    Removing unnecessary service.

--- src/automowerPlatformContainer.ts
+++ src/automowerPlatformContainer.ts
@@ -5,7 +5,6 @@ import { AutomowerPlatformConfig } from './automowerPlatformConfig';
 import { AuthenticationClientImpl } from './clients/impl/authenticationClientImpl';
 import { AutomowerClientImpl } from './clients/impl/automowerClientImpl';
 import { GetMowersServiceImpl } from './services/automower/impl/getMowersServiceImpl';
-import { PauseMowerServiceImpl } from './services/automower/impl/pauseMowerServiceImpl';
 import { DiscoveryServiceImpl } from './services/impl/discoveryServiceImpl';
 import * as constants from './constants';
 
@@ -42,13 +41,6 @@ export class AutomowerPlatformContainer {
 
         this.container.register(GetMowersServiceImpl, {
             useFactory: (context) => new GetMowersServiceImpl(
-                context.resolve(OAuthTokenManagerImpl),
-                context.resolve(AutomowerClientImpl)
-            )
-        });
-
-        this.container.register(PauseMowerServiceImpl, {
-            useFactory: (context) => new PauseMowerServiceImpl(
                 context.resolve(OAuthTokenManagerImpl),
                 context.resolve(AutomowerClientImpl)
             )
