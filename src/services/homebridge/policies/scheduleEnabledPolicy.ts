937193f68cb58d6d4c1594275da326f47034ec3c
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-05-30 11:13:46 -0400

    Changing policy to ignore when mower is in use.

--- src/services/homebridge/policies/scheduleEnabledPolicy.ts
+++ src/services/homebridge/policies/scheduleEnabledPolicy.ts
@@ -1,6 +1,9 @@
 import { InvalidStateError } from '../../../errors/invalidStateError';
-import { Calendar, MowerState, Planner, RestrictedReason } from '../../../model';
+import { Calendar, MowerState, Planner, RestrictedReason, State } from '../../../model';
 
+/**
+ * A policy which decides whether the mower schedule is enabled.
+ */
 export interface ScheduleEnabledPolicy {
     /**
      * Identifies whether the policy should be applied.
@@ -40,7 +43,8 @@ export class DeterministicScheduleEnabledPolicy implements ScheduleEnabledPolicy
     private mowerState?: MowerState;
 
     public shouldApply(): boolean {
-        return this.calendar !== undefined && this.planner !== undefined;
+        return this.calendar !== undefined && this.planner !== undefined &&
+            (this.mowerState === undefined || this.mowerState.state !== State.IN_OPERATION);
     }
 
     public apply(): boolean {
@@ -48,14 +52,19 @@ export class DeterministicScheduleEnabledPolicy implements ScheduleEnabledPolicy
             throw new InvalidStateError('The calendar and planner are both required.');
         }
         
+        // Checks whether any days have been enabled on any of the schedules.
         let anyDaysEnabled = false;        
         this.calendar.tasks.forEach(task => {
             if (task.sunday || task.monday || task.tuesday || task.wednesday || task.thursday || task.friday || task.saturday) {
                 anyDaysEnabled = true;
             }
         });
-    
-        return anyDaysEnabled && this.planner.restrictedReason === RestrictedReason.WEEK_SCHEDULE;
+
+        // Checks whether the mower is waiting to run in the future (seen when in charge station).
+        const isFutureScheduled = this.planner.nextStartTimestamp > 0 && 
+            this.planner.restrictedReason === RestrictedReason.WEEK_SCHEDULE;        
+
+        return anyDaysEnabled && isFutureScheduled;
     }
     
     public setCalendar(calendar: Calendar): void {
--- test/services/homebridge/policies/scheduleEnabledPolicy.spec.ts
+++ test/services/homebridge/policies/scheduleEnabledPolicy.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidStateError } from '../../../../src/errors/invalidStateError';
-import { Calendar, RestrictedReason } from '../../../../src/model';
+import { Activity, Calendar, Mode, RestrictedReason, State } from '../../../../src/model';
 import { DeterministicScheduleEnabledPolicy } from '../../../../src/services/homebridge/policies/scheduleEnabledPolicy';
 
 describe('DeterministicScheduleEnabledPolicy', () => {
@@ -7,6 +7,68 @@ describe('DeterministicScheduleEnabledPolicy', () => {
 
     beforeEach(() => {
         target = new DeterministicScheduleEnabledPolicy();
+    });
+
+    it('should return true when the mower state is undefined', () => {
+        target.setCalendar({
+            tasks: [
+                {
+                    start: 1,
+                    duration: 1,
+                    sunday: false,
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false
+                }
+            ]
+        });
+        
+        target.setPlanner({
+            nextStartTimestamp: 0,
+            override: { },
+        });
+
+        const result = target.shouldApply();
+
+        expect(result).toBeTruthy();
+    });
+
+    it('should return false when the mower is in operation', () => {
+        target.setCalendar({
+            tasks: [
+                {
+                    start: 1,
+                    duration: 1,
+                    sunday: false,
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false
+                }
+            ]
+        });
+        
+        target.setPlanner({
+            nextStartTimestamp: 0,
+            override: { },
+        });
+
+        target.setMowerState({
+            activity: Activity.MOWING,
+            errorCode: 0,
+            errorCodeTimestamp: 0,
+            mode: Mode.MAIN_AREA,
+            state: State.IN_OPERATION
+        });
+
+        const result = target.shouldApply();
+
+        expect(result).toBeFalsy();
     });
 
     it('should return false when nothing is set', () => {
