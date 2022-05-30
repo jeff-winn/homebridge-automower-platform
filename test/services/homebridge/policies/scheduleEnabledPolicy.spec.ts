aaae5e08bf42a8d5296a4618eb2581b5185d43cc
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-05-30 11:25:36 -0400

    Adding unit tests.

--- test/services/homebridge/policies/scheduleEnabledPolicy.spec.ts
+++ test/services/homebridge/policies/scheduleEnabledPolicy.spec.ts
@@ -112,7 +112,7 @@ describe('DeterministicScheduleEnabledPolicy', () => {
         expect(result).toBeFalsy();
     });
     
-    it('should update the characteristic as false when calendar is not scheduled to start', () => {
+    it('should return false when calendar is not scheduled to start', () => {
         const calendar: Calendar = {
             tasks: [
                 {
@@ -141,7 +141,102 @@ describe('DeterministicScheduleEnabledPolicy', () => {
         expect(result).toBeFalsy();
     });
 
-    it('should update the characteristic as false when calendar and planner is not scheduled to start', () => {
+    it('should return false when the planner has park overridden', () => {
+        target.setCalendar({
+            tasks: [
+                {
+                    start: 1,
+                    duration: 1,
+                    sunday: true,
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false
+                }
+            ]
+        });
+
+        target.setPlanner({
+            nextStartTimestamp: 0,
+            override: { },
+            restrictedReason: RestrictedReason.PARK_OVERRIDE
+        });
+
+        const result = target.apply();
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return true when the only task is scheduled to start', () => {
+        target.setCalendar({
+            tasks: [
+                {
+                    start: 1,
+                    duration: 1,
+                    sunday: true,
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false
+                }
+            ]
+        });
+
+        target.setPlanner({
+            nextStartTimestamp: 0,
+            override: { },
+            restrictedReason: RestrictedReason.WEEK_SCHEDULE
+        });
+
+        const result = target.apply();
+
+        expect(result).toBeTruthy();
+    });
+
+    it('should return true when one of the calendar tasks is scheduled to start', () => {
+        target.setCalendar({
+            tasks: [
+                {
+                    start: 1,
+                    duration: 1,
+                    sunday: false,
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false
+                },
+                {
+                    start: 1,
+                    duration: 1,
+                    sunday: true,
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false
+                }
+            ]
+        });
+
+        target.setPlanner({
+            nextStartTimestamp: 0,
+            override: { },
+            restrictedReason: RestrictedReason.WEEK_SCHEDULE
+        });
+
+        const result = target.apply();
+
+        expect(result).toBeTruthy();
+    });
+
+    it('should return false when calendar is not scheduled to start', () => {
         target.setCalendar({
             tasks: [
                 {
@@ -161,7 +256,7 @@ describe('DeterministicScheduleEnabledPolicy', () => {
         target.setPlanner({
             nextStartTimestamp: 0,
             override: { },
-            restrictedReason: RestrictedReason.NOT_APPLICABLE
+            restrictedReason: RestrictedReason.WEEK_SCHEDULE
         });
 
         const result = target.apply();
