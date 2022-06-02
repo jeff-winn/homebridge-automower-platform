506c019c6bd64dc87dfe8fbf695f3473d9a4ad1c
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-06-01 22:14:26 -0400

    Adding retryer support within the node client.

--- src/clients/fetchClient.ts
+++ src/clients/fetchClient.ts
@@ -2,7 +2,6 @@ import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
 import { v4 as uuid } from 'uuid';
 
 import { PlatformLogger } from '../diagnostics/platformLogger';
-
 export { Response, RequestInfo, RequestInit } from 'node-fetch';
 
 /**
@@ -18,12 +17,48 @@ export interface FetchClient {
 }
 
 /**
- * A client which uses node-fetch to perform HTTP requests.
+ * A client which uses node-fetch to perform HTTP requests and includes retryer support.
  */
-export class DefaultFetchClient implements FetchClient {
-    public constructor(private log: PlatformLogger) { }
+export class RetryerFetchClient implements FetchClient {
+    public constructor(private log: PlatformLogger, private maxRetryAttempts: number, private maxDelayInMillis: number) { }
 
     public async execute(url: RequestInfo, init?: RequestInit): Promise<Response> {
+        let retry = false;
+        let response: Response;
+        let attempts = 0;
+
+        do {
+            retry = false;
+            attempts++;
+
+            response = await this.executeCore(url, init);
+            if (response.status === 429) {
+                await this.onTooManyRequests();
+                retry = true;
+            }
+        } while (retry && attempts <= this.maxRetryAttempts);
+        
+        return response;
+    }
+
+    private async onTooManyRequests(): Promise<void> {
+        const delay = this.rand(0, this.maxDelayInMillis);
+        await this.sleep(delay);
+    }
+
+    private rand(min: number, max: number) {
+        return Math.floor(
+            Math.random() * (max - min + 1) + max
+        );
+    }
+
+    private sleep(ms: number): Promise<unknown> {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    protected async executeCore(url: RequestInfo, init?: RequestInit): Promise<Response> {
         const id = uuid();
 
         this.log.debug(`Sending request: ${id}\r\n`, JSON.stringify({
@@ -33,7 +68,7 @@ export class DefaultFetchClient implements FetchClient {
             body: init?.body        
         }));
 
-        const response = await fetch(url, init);
+        const response = await this.doFetch(url, init);
         const buffer = await response.buffer();
 
         this.log.debug(`Received response: ${id}\r\n`, JSON.stringify({
@@ -52,5 +87,9 @@ export class DefaultFetchClient implements FetchClient {
             timeout: response.timeout,
             url: response.url
         });
+    }
+
+    protected doFetch(url: RequestInfo, init?: RequestInit): Promise<Response> {
+        return fetch(url, init);
     }
 }
