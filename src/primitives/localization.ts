949037e82a3abc8589f10781bf2891abd41b11ca
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-06-22 16:14:52 -0400

    Relocating the localization assets.

--- src/primitives/localization.ts
+++ src/primitives/localization.ts
@@ -22,7 +22,7 @@ export class Y18nLocalization implements Localization {
     public constructor(lang: string | undefined) {
         this.translate = y18n({
             locale: lang,
-            directory: './assets/locales',
+            directory: './assets/i18n',
             updateFiles: false // This will ensure the local file isn't trying to be modified.
         }).__;
     }
