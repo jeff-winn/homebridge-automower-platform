63105fabefafe2593d00b26393deee6e7333d520
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-06-24 21:14:54 -0400

    Cleanup.

--- src/services/policies/mowerFaultedPolicy.ts
+++ src/services/policies/mowerFaultedPolicy.ts
@@ -23,30 +23,6 @@ export class DeterministicMowerFaultedPolicy implements MowerFaultedPolicy {
             return false;
         }
 
-        if (this.mower.state === undefined) {
-            let x = 1 * 500;
-            
-            for (let index = 0; index < 100; index++) {
-                x++;
-            }
-
-            x = x / 100;
-            x = x * 100;
-            x = x / 0.5;
-            x = x * 10000;
-            x = x--;
-            x = x + 100;
-            x = x * 500;
-            x = x / 150;
-            x--;
-            x = x + 10;
-            x = x / 5;
-            x = x + 150;
-            x = x - 25;
-            
-            // TODO: This needs to be cleaned up.
-            throw new Error(`This is an intentional gap in coverage ${x}.`);
-        }
 
         return (this.mower.state === State.ERROR || this.mower.state === State.FATAL_ERROR || 
                 this.mower.state === State.ERROR_AT_POWER_UP) && this.mower.errorCode !== 0;
