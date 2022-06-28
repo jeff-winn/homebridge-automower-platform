771da032f93fffcf94dff619776e6af7a316f09d
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-06-27 21:15:54 -0400

    Adding policy to check whether mower is arriving.

---
+++ test/services/policies/mowerIsArrivingPolicy.spec.ts
@@ -0,0 +1,58 @@
+import { Activity, Mode, State } from '../../../src/model';
+import { DeterministicMowerIsArrivingPolicy } from '../../../src/services/policies/mowerIsArrivingPolicy';
+
+describe('DeterministicMowerIsArrivingPolicy', () => {
+    let target: DeterministicMowerIsArrivingPolicy;
+
+    beforeEach(() => {
+        target = new DeterministicMowerIsArrivingPolicy();
+    });
+
+    it('should return false when the mower state is not set', () => {
+        const result = target.check();
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return false when the mower state is not going home', () => {
+        target.setMowerState({
+            activity: Activity.MOWING,
+            errorCode: 0,
+            errorCodeTimestamp: 0,
+            mode: Mode.MAIN_AREA,
+            state: State.IN_OPERATION
+        });
+
+        const result = target.check();
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return true when the mower state is going home but not in operation', () => {
+        target.setMowerState({
+            activity: Activity.GOING_HOME,
+            errorCode: 0,
+            errorCodeTimestamp: 0,
+            mode: Mode.MAIN_AREA,
+            state: State.STOPPED
+        });
+
+        const result = target.check();
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return true when the mower state is going home', () => {
+        target.setMowerState({
+            activity: Activity.GOING_HOME,
+            errorCode: 0,
+            errorCodeTimestamp: 0,
+            mode: Mode.MAIN_AREA,
+            state: State.IN_OPERATION
+        });
+
+        const result = target.check();
+
+        expect(result).toBeTruthy();
+    });
+});
