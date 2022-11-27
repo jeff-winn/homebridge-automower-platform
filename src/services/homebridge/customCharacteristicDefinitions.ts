66d3122ed75aed0c8f5749fbfcfba5b7d4e45fbb
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-11-26 21:21:05 -0500

    Removing characteristics.

--- src/services/homebridge/customCharacteristicDefinitions.ts
+++ src/services/homebridge/customCharacteristicDefinitions.ts
@@ -1,17 +1,5 @@
-import { ChargingCycles } from './characteristics/chargingCycles';
-import { TotalChargingTime } from './characteristics/totalChargingTime';
-
 /**
  * Defines the custom characteristic definitions available within the platform.
  */
 export class CustomCharacteristicDefinitions {
-    /**
-     * The number of charging cycles.
-     */
-    public static readonly ChargingCycles = ChargingCycles;
-
-    /**
-     * The total time spent charging the device.
-     */
-    public static readonly TotalChargingTime = TotalChargingTime;
 }
