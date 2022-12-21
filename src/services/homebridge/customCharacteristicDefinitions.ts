5ff18b58c063e3f5fbc04d212dc0a5e8e97586e1
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-12-21 13:06:01 -0500

    Fixing issue with the plugin failing to load. (#249)
    * Fixing issue with the plugin failing to load.
    
    * Fixing preview version identifiers.

--- src/services/homebridge/customCharacteristicDefinitions.ts
+++
@@ -1,8 +0,0 @@
-import { CuttingHeight } from './characteristics/cuttingHeight';
-
-/**
- * Defines the custom characteristic definitions available within the platform.
- */
-export class CustomCharacteristicDefinitions {
-    public static readonly CuttingHeight = CuttingHeight;
-}
