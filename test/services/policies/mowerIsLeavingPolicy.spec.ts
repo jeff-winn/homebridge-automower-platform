9343ce31e7aca00b84ca5314447e657b2e729dd7
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2023-03-04 15:49:38 -0500

    Fixing unit tests.

--- test/services/policies/mowerIsLeavingPolicy.spec.ts
+++ test/services/policies/mowerIsLeavingPolicy.spec.ts
@@ -1,4 +1,4 @@
-import { Activity, Mode, State } from '../../../src/clients/automower/automowerClient';
+import { Activity, State } from '../../../src/model';
 import { DeterministicMowerIsLeavingPolicy } from '../../../src/services/policies/mowerIsLeavingPolicy';
 
 describe('DeterministicMowerIsLeavingPolicy', () => {
@@ -14,45 +14,94 @@ describe('DeterministicMowerIsLeavingPolicy', () => {
         expect(result).toBeFalsy();
     });
 
-    it('should return false when the mower state is in operation', () => {
+    it('should return true when leaving home and in operation', () => {
         target.setMowerState({
-            activity: Activity.MOWING,
-            errorCode: 0,
-            errorCodeTimestamp: 0,
-            mode: Mode.MAIN_AREA,
-            state: State.IN_OPERATION
-        });
-
-        const result = target.check();
-
-        expect(result).toBeFalsy();
-    });
-
-    it('should return true when the mower state is leaving but not in operation', () => {
-        target.setMowerState({
-            activity: Activity.LEAVING,
-            errorCode: 0,
-            errorCodeTimestamp: 0,
-            mode: Mode.MAIN_AREA,
-            state: State.STOPPED
-        });
-
-        const result = target.check();
-
-        expect(result).toBeFalsy();
-    });
-
-    it('should return true when the mower state is leaving', () => {
-        target.setMowerState({
-            activity: Activity.LEAVING,
-            errorCode: 0,
-            errorCodeTimestamp: 0,
-            mode: Mode.MAIN_AREA,
-            state: State.IN_OPERATION
+            activity: Activity.LEAVING_HOME,
+            state: State.IN_OPERATION,
+            enabled: true
         });
 
         const result = target.check();
 
         expect(result).toBeTruthy();
     });
+
+    it('should return false when leaving home and paused', () => {
+        target.setMowerState({
+            activity: Activity.LEAVING_HOME,
+            state: State.PAUSED,
+            enabled: true
+        });
+
+        const result = target.check();
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return false when leaving home and faulted', () => {
+        target.setMowerState({
+            activity: Activity.LEAVING_HOME,
+            state: State.FAULTED,
+            enabled: true
+        });
+
+        const result = target.check();
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return false when leaving home and tampered', () => {
+        target.setMowerState({
+            activity: Activity.LEAVING_HOME,
+            state: State.TAMPERED,
+            enabled: true
+        });
+
+        const result = target.check();
+
+        expect(result).toBeFalsy();
+    });
+
+    // TODO: Clean this up.
+    // it('should return false when the mower state is in operation', () => {
+    //     target.setMowerState({
+    //         activity: Activity.MOWING,
+    //         errorCode: 0,
+    //         errorCodeTimestamp: 0,
+    //         mode: Mode.MAIN_AREA,
+    //         state: State.IN_OPERATION
+    //     });
+
+    //     const result = target.check();
+
+    //     expect(result).toBeFalsy();
+    // });
+
+    // it('should return true when the mower state is leaving but not in operation', () => {
+    //     target.setMowerState({
+    //         activity: Activity.LEAVING,
+    //         errorCode: 0,
+    //         errorCodeTimestamp: 0,
+    //         mode: Mode.MAIN_AREA,
+    //         state: State.STOPPED
+    //     });
+
+    //     const result = target.check();
+
+    //     expect(result).toBeFalsy();
+    // });
+
+    // it('should return true when the mower state is leaving', () => {
+    //     target.setMowerState({
+    //         activity: Activity.LEAVING,
+    //         errorCode: 0,
+    //         errorCodeTimestamp: 0,
+    //         mode: Mode.MAIN_AREA,
+    //         state: State.IN_OPERATION
+    //     });
+
+    //     const result = target.check();
+
+    //     expect(result).toBeTruthy();
+    // });
 });
