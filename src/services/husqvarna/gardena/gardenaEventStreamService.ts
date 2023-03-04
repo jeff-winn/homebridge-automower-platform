1abf79611c4aee71720e7b144cee23352c826394
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2023-03-04 14:40:51 -0500

    Updating todo statements.

--- src/services/husqvarna/gardena/gardenaEventStreamService.ts
+++ src/services/husqvarna/gardena/gardenaEventStreamService.ts
@@ -3,24 +3,24 @@ import { EventStreamService } from '../eventStreamService';
 
 export class GardenaEventStreamService implements EventStreamService {
     public onPositionsEventReceived(callback: (event: PositionsEvent) => Promise<void>): void {
-        // TODO: This method intentionally left blank.
+        // TODO: This method needs to be implemented.
     }
 
     public onStatusEventReceived(callback: (event: StatusEvent) => Promise<void>): void {
-        // TODO: This method intentionally left blank.
+        // TODO: This method needs to be implemented.
     }
 
     public onSettingsEventReceived(callback: (event: SettingsEvent) => Promise<void>): void {
-        // TODO: This method intentionally left blank.
+        // TODO: This method needs to be implemented.
     }
 
     public start(): Promise<void> {
-        // TODO: This method intentionally left blank.
+        // TODO: This method needs to be implemented.
         return Promise.resolve();
     }
 
     public stop(): Promise<void> {
-        // TODO: This method intentionally left blank.
+        // TODO: This method needs to be implemented.
         return Promise.resolve();
     }
 }
