1fbb90581c772d7acc9409080b604c4bb1ae6139
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2023-03-05 10:07:50 -0500

    Adding unit tests.

--- test/services/husqvarna/gardena/gardenaGetMowersService.spec.ts
+++ test/services/husqvarna/gardena/gardenaGetMowersService.spec.ts
@@ -1,9 +1,12 @@
-import { Mock, Times } from 'moq.ts';
+import { It, Mock, Times } from 'moq.ts';
 
-import { GardenaClient, ItemType, LocationDataItem } from '../../../../src/clients/gardena/gardenaClient';
+import { 
+    BatteryState, CommonServiceDataItem, DeviceDataItem, GardenaClient, ItemType, MowerActivity, 
+    MowerError, MowerServiceDataItem, RFLinkState, ServiceState 
+} from '../../../../src/clients/gardena/gardenaClient';
 import { PlatformLogger } from '../../../../src/diagnostics/platformLogger';
 import { NotAuthorizedError } from '../../../../src/errors/notAuthorizedError';
-import { AccessToken } from '../../../../src/model';
+import { AccessToken, Activity, State } from '../../../../src/model';
 import { AccessTokenManager } from '../../../../src/services/husqvarna/accessTokenManager';
 import { GardenaGetMowersService } from '../../../../src/services/husqvarna/gardena/gardenaGetMowersService';
 
@@ -57,38 +60,152 @@ describe('GardenaGetMowersService', () => {
             value: '12345'
         };
 
-        const locationRef: LocationDataItem = {
-            id: 'abcd1234',
-            type: ItemType.LOCATION,
-            attributes: {
-                name: 'Tommy Boy'
+        const mowerDevice: DeviceDataItem = {
+            id: '12345',
+            type: ItemType.DEVICE,
+            relationships: {
+                location: {
+                    data: {
+                        id: 'abcd1234',
+                        type: ItemType.LOCATION
+                    }
+                },
+                services: {
+                    data: [
+                        {
+                            id: '12345',
+                            type: ItemType.MOWER
+                        },
+                        {
+                            id: '12345',
+                            type: ItemType.COMMON
+                        }
+                    ]
+                }
             }
+        };
+
+        const mowerService: MowerServiceDataItem = {
+            id: '12345',
+            type: ItemType.MOWER,
+            relationships: {
+                data: {
+                    id: '12345',
+                    type: ItemType.DEVICE
+                }
+            },
+            attributes: {
+                state: {
+                    value: ServiceState.WARNING,
+                    timestamp: '2022-12-09T09:59:16.505+00:00'
+                },
+                activity: {
+                    value: MowerActivity.OK_CHARGING,
+                    timestamp: '2022-12-09T09:59:16.505+00:00'
+                },
+                lastErrorCode: {
+                    value: MowerError.OFF_DISABLED,
+                    timestamp: '2022-12-09T09:59:16.505+00:00'
+                },
+                operatingHours: {
+                    value: 12345
+                }
+            }
+        };
+
+        const commonService: CommonServiceDataItem = {
+            id: '12345',
+            type: ItemType.COMMON,
+            relationships: {
+                data: {
+                    id: '12345',
+                    type: ItemType.DEVICE
+                }
+            },
+            attributes: {
+                name: {
+                    value: 'SILENO'
+                },
+                batteryLevel: {
+                    value: 100,
+                    timestamp: '2022-12-09T09:59:16.505+00:00'
+                },
+                batteryState: {
+                    value: BatteryState.OK,
+                    timestamp: '2022-12-09T09:59:16.505+00:00'
+                },
+                rfLinkLevel: {
+                    value: 90,
+                    timestamp: '2022-12-09T09:59:16.505+00:00'
+                },
+                serial: {
+                    value: '1234567890'
+                },
+                modelType: {
+                    value: 'GARDENA smart Mower'
+                },
+                rfLinkState: {
+                    value: RFLinkState.ONLINE
+                }
+            }            
         };
 
         tokenManager.setup(o => o.getCurrentToken()).returnsAsync(token);
         client.setup(o => o.getLocations(token)).returnsAsync({
             data: [
-                locationRef
+                {
+                    id: 'abcd1234',
+                    type: ItemType.LOCATION,
+                    attributes: {
+                        name: 'My Garden'
+                    }
+                }
             ]
         });
+
         client.setup(o => o.getLocation('abcd1234', token)).returnsAsync({
             data: {
                 id: 'abcd1234',
-                type: ItemType.DEVICE,
-                attributes: {
-                    name: 'My Garden'
-                },
+                type: ItemType.LOCATION,
                 relationships: {
                     devices: {
-                        data: []
+                        data: [
+                            {
+                                id: '12345',
+                                type: ItemType.DEVICE
+                            }
+                        ]
                     }
+                },
+                attributes: {
+                    name: 'My Garden'
                 }
             },
-            included: []
+            included: [
+                mowerDevice,
+                mowerService,
+                commonService
+            ]
         });
 
-        const result = await target.getMowers();
-        expect(result).toBeDefined();
+        log.setup(o => o.debug(It.IsAny(), It.IsAny())).returns(undefined);
+        log.setup(o => o.warn(It.IsAny(), It.IsAny())).returns(undefined);
+
+        const mowers = await target.getMowers();
+        expect(mowers).toBeDefined();
+
+        const result = mowers[0];
+        expect(result.id).toBe('12345');
+        expect(result.attributes.battery.level).toBe(100);
+        expect(result.attributes.connection.connected).toBeTruthy();
+        expect(result.attributes.location!.id).toBe('abcd1234');
+        expect(result.attributes.metadata.manufacturer).toBe('GARDENA');
+        expect(result.attributes.metadata.model).toBe('smart Mower');
+        expect(result.attributes.metadata.name).toBe('SILENO');
+        expect(result.attributes.metadata.serialNumber).toBe('1234567890');
+        expect(result.attributes.mower.activity).toBe(Activity.CHARGING);
+        expect(result.attributes.mower.enabled).toBeFalsy();
+        expect(result.attributes.mower.state).toBe(State.OFF);
 
         log.verify(o => o.warn('GARDENA_PREVIEW_IN_USE'), Times.Once());
     });
