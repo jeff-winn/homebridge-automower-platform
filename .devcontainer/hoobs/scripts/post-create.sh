3f219bfda2e02eb049d7ea8d10dbc4f02ebd1b09
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2023-03-21 23:42:51 -0400

    Enabling HOOBS debugging.

--- .devcontainer/hoobs/scripts/post-create.sh
+++ .devcontainer/hoobs/scripts/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f /etc/hoobs/bridges.conf ]; then
-    /usr/bin/hbs install -p 8080 && \
-    /usr/bin/hbs bridge create -b default -p 50826 -a bonjour
+    /usr/bin/hbs install -p 8080
+    /usr/bin/hbs bridge create -b local -p 50826 -a bonjour
 fi
